b8603561c09cbd88a74574ae3e57a049d6470431
nttgamer117-rgb <nttgamer117@gmail.com>
2026-09-14 01:01:20 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,9 @@
-const CACHE = "neetos-pwa-v1";
+const CACHE = "neetos-pwa-v2";
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./", "./index.html", "./style.css", "./app.js",
+  "./manifest.webmanifest", "./manifest.json", "./favicon.png",
+  "./icons/icon-192.png", "./icons/icon-512.png",
+  "./icons/icon-192-maskable.png", "./icons/icon-512-maskable.png"
 ];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
@@ -17,8 +14,6 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
   event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-    const copy = response.clone();
-    caches.open(CACHE).then(cache => cache.put(event.request, copy));
-    return response;
+    const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); return response;
   }).catch(() => caches.match("./index.html"))));
 });
